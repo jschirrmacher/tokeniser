f0cd35471d91b2321bf3260c6aba541faa14155f
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2017-09-29 08:21:17 +0200

    Fixed tests

--- test/tokeniser.test.js
+++ test/tokeniser.test.js
@@ -9,7 +9,6 @@ describe('Tokeniser', () => {
         result.should.be.an.array
         result.length.should.equal(1)
         result[0].type.should.equal('comment')
-        result[0].matches[0].should.equal('// abc')
         done()
     })
 
@@ -17,10 +16,10 @@ describe('Tokeniser', () => {
         let result = tokeniser('Name = Test\nAuthor = "Joachim Schirrmacher"', rules)
         result[0].type.should.equal('definition')
         result[1].type.should.equal('definition')
-        result[0].matches[1].should.equal('Name')
-        result[0].matches[2].should.equal('Test')
-        result[1].matches[1].should.equal('Author')
-        result[1].matches[2].should.equal('"Joachim Schirrmacher"')
+        result[0].matches[0].should.equal('Name')
+        result[0].matches[1].should.equal('Test')
+        result[1].matches[0].should.equal('Author')
+        result[1].matches[1].should.equal('"Joachim Schirrmacher"')
         done()
     })
 
@@ -29,10 +28,8 @@ describe('Tokeniser', () => {
         result.should.be.an.array
         result.length.should.equal(3)
         result[0].type.should.equal('arrayStart')
-        result[0].matches[0].should.equal('abc [')
-        result[0].matches[1].should.equal('abc')
+        result[0].matches[0].should.equal('abc')
         result[2].type.should.equal('arrayEnd')
-        result[2].matches[0].trim().should.equal(']')
         done()
     })
 
@@ -41,10 +38,8 @@ describe('Tokeniser', () => {
         result.should.be.an.array
         result.length.should.equal(3)
         result[0].type.should.equal('objectStart')
-        result[0].matches[0].should.equal('abc {')
-        result[0].matches[1].should.equal('abc')
+        result[0].matches[0].should.equal('abc')
         result[2].type.should.equal('objectEnd')
-        result[2].matches[0].trim().should.equal('}')
         done()
     })
 
@@ -55,14 +50,11 @@ describe('Tokeniser', () => {
         result[1].type.should.equal('objectStart')
         result[2].type.should.equal('objectEnd')
         result[3].type.should.equal('arrayEnd')
-        result[0].matches[0].should.equal('Array[')
-        result[0].matches[1].should.equal('Array')
-        result[1].matches[0].should.equal('\n\t{')
-        result[1].matches[1].should.equal('')
-        result[2].matches[0].should.equal('\n\t}')
-        result[3].matches[0].should.equal('\n]')
+        result[0].matches[0].should.equal('Array')
+        result[1].matches[0].should.equal('')
         done()
     })
+
     it('should recognise arrays in an array', done => {
         let result = tokeniser('Array[\n\t[\n\t]\n]', rules)
         result.length.should.equal(4)
@@ -70,12 +62,8 @@ describe('Tokeniser', () => {
         result[1].type.should.equal('arrayStart')
         result[2].type.should.equal('arrayEnd')
         result[3].type.should.equal('arrayEnd')
-        result[0].matches[0].should.equal('Array[')
-        result[0].matches[1].should.equal('Array')
-        result[1].matches[0].should.equal('\n\t[')
-        result[1].matches[1].should.equal('')
-        result[2].matches[0].should.equal('\n\t]')
-        result[3].matches[0].should.equal('\n]')
+        result[0].matches[0].should.equal('Array')
+        result[1].matches[0].should.equal('')
        done()
     })
 
@@ -84,10 +72,8 @@ describe('Tokeniser', () => {
         result.should.be.an.array
         result.length.should.equal(3)
         result[0].type.should.equal('arrayStart')
-        result[0].matches[0].should.equal('abc [')
-        result[0].matches[1].should.equal('abc')
+        result[0].matches[0].should.equal('abc')
         result[2].type.should.equal('arrayEnd')
-        result[2].matches[0].trim().should.equal(']')
         done()
     })
 })
